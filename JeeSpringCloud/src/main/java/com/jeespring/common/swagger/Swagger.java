b3d52ed163a7ffb9c46b677051ab9e8927d151be
HuangBingGui <516821420@qq.com>
2018-10-24 16:32:53 +0800

    no commit message

--- JeeSpringCloud/src/main/java/com/jeespring/common/swagger/Swagger.java
+++ JeeSpringCloud/src/main/java/com/jeespring/common/swagger/Swagger.java
@@ -21,17 +21,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger {
-    @Bean
-    public Docket createRestApi() {
+
+    @Bean("JeeSpring云接口")
+    public Docket createJeeSpringRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-        .apiInfo(apiInfo())
-        .select()
-        //.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))                         //这里采用包含注解的方式来确定要显示的接口
-        //.apis(RequestHandlerSelectors.basePackage("com.jeespring.modules"))    //这里采用包扫描的方式来确定要显示的接口
-        .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
-        .paths(PathSelectors.any())
-        .build();
+                .groupName("JeeSpring云接口")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))                         //这里采用包含注解的方式来确定要显示的接口
+                //.apis(RequestHandlerSelectors.basePackage("com.jeespring.modules"))    //这里采用包扫描的方式来确定要显示的接口
+                //.apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //.paths(PathSelectors.regex("/rest/.*"))
+                //.paths(PathSelectors.any())
+                .build();
     }
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Swagger2构建RESTful APIs")
