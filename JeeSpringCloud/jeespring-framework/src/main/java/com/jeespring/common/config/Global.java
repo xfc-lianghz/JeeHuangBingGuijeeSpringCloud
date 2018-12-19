c3a796b099154b22d3878f9251780e6e14ecc084
Suncos <516821420@qq.com>
2018-12-19 14:38:16 +0800

    no commit message

--- JeeSpringCloud/jeespring-framework/src/main/java/com/jeespring/common/config/Global.java
+++ JeeSpringCloud/jeespring-framework/src/main/java/com/jeespring/common/config/Global.java
@@ -185,6 +185,7 @@ public class Global {
             String url = resolver.getProperty("spring.datasource.url");
             //String url = env.getProperty("spring.datasource.url");
             String type = getDbType(url);
+			if(type.equals("sqlserver")){type="mssql";}
             map.put("spring.datasource.url", type);
             return type;
         } catch (Exception e) {
